021e61ce8ec459416a3576b8276bb1d279a40372
xuir268 <sparshgupta521@gmail.com>
2026-01-03 23:51:16 +0530

    added app file

--- src/app.h
+++ src/app.h
@@ -1,2 +1,15 @@
 #pragma once
 #include <vector>
+#include "router.hpp"
+#include "middleware.hpp"
+
+class App {
+public:
+  App(Router router, std::vector<Middleware> mws);
+
+  void handle(ReqContext& ctx, const Request& req, Response& res);
+
+private:
+  Router router_;
+  std::vector<Middleware> mws_;
+};
